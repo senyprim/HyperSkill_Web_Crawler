bdb2143b40aeab7066c153db19cfb0a016b46e5e
senyprim <senyprim105@gmail.com>
2020-07-26 17:58:52 +1000

    Stage 1

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,11 +1,28 @@
 package crawler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
+        super();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+        setTitle("Simple Window");
+        setBackground(Color.GRAY);
+        JPanel workFlow = new JPanel();
+        workFlow.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+
+        JTextArea textArea = new JTextArea();
+        textArea.setName("TextArea");
+        textArea.setText("HTML code?");
+        textArea.setEnabled(false);
+        textArea.setPreferredSize(new Dimension(200,200));
+
+        textArea.setBackground(Color.WHITE);
+        workFlow.add(textArea);
+        add(workFlow);
+
         setVisible(true);
     }
 }
