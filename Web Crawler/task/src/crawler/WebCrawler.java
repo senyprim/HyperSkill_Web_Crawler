914b660738f94d104392408ecaaefda23a4ed258
senyprim <senyprim105@gmail.com>
2020-07-27 02:49:17 +1000

    Stage 4

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -3,6 +3,8 @@ package crawler;
 import org.intellij.lang.annotations.JdkConstants;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,20 +15,24 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class WebCrawler extends JFrame {
+    private String text;
     public WebCrawler() {
         super();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setTitle("Web Crawler");
         setBackground(Color.GRAY);
+
         JPanel workFlow = new JPanel();
         workFlow.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         workFlow.setLayout(new BorderLayout());
-
 
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel,BoxLayout.X_AXIS));
@@ -56,19 +62,16 @@ public class WebCrawler extends JFrame {
         title.setName("TitleLabel");
         titlePanel.add(title);
 
-        JTextArea textArea = new JTextArea();
-        textArea.setName("HtmlTextArea");
-        textArea.setEnabled(false);
-        textArea.setPreferredSize(new Dimension(200,200));
-        textArea.setBackground(Color.WHITE);
-        centerPanel.add(textArea,BorderLayout.CENTER);
+        JTable table = createTable();
+        table.setBackground(Color.WHITE);
+        centerPanel.add(new JScrollPane(table),BorderLayout.CENTER);
 
         add(workFlow);
 
         button.addActionListener( event->{
                 try {
-                    textArea.setText(loadUrl(textField.getText()));
-                    title.setText(getTitle(textArea.getText()));
+                    text=(loadUrl(textField.getText()));
+                    title.setText(getTitle(text));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -90,6 +93,26 @@ public class WebCrawler extends JFrame {
             return new String(input.readAllBytes(), StandardCharsets.UTF_8);
         }
     }
+    private JTable createTable(){
+        String[] title = {"URL","Title"};
+        JTable jTable = new JTable();
+        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.setColumnIdentifiers(title);
+        tableModel.addRow(new String[]{"",""});
+        return new JTable(tableModel);
+    }
+    private Stream<String[]> getLinks(String text){
+        Matcher matcher = Pattern.compile("<\\s*a[^>]*href\\s*=\\s*['\"]+([^\\'\" >]+)[\\'\"]>(.*)\\<\\s*\\/a\\s*>"
+                ,Pattern.CASE_INSENSITIVE|Pattern.MULTILINE).matcher(text);
+        List<String[]> result = new ArrayList();
+        while (matcher.find()){
+            result.add(new String[]{matcher.group(1),matcher.group(2)});
+        }
+        return result.stream();
+    }
+    
+
+
 
 
 }
