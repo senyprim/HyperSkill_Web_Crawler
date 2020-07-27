c64f0407f8513efb11bb9c0c401d2be3006e1524
senyprim <senyprim105@gmail.com>
2020-07-27 20:39:48 +1000

    Stage 5

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,39 +1,25 @@
 package crawler;
 
-import kotlin.text.Regex;
-import org.intellij.lang.annotations.JdkConstants;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class WebCrawler extends JFrame {
 
-
-    JLabel title;
+    JLabel titleLabel;
     private String text;
     private String url;
-    private String textTitle;
-    private String[][] data;
+    private String title;
 
 
     public WebCrawler() {
@@ -42,50 +28,68 @@ public class WebCrawler extends JFrame {
         setSize(300, 300);
         setTitle("Web Crawler");
         setBackground(Color.GRAY);
-
+        //Добавляем рабочее пространство с бордером в 5пикселей
         JPanel workFlow = new JPanel();
         workFlow.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         workFlow.setLayout(new BorderLayout());
-
+        //На верх добавляем панель
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel,BoxLayout.X_AXIS));
         topPanel.setBorder(BorderFactory.createEmptyBorder(0,0,5,0));
-
+        //С текстовым полем
         JTextField textField = new JTextField();
         textField.setName("UrlTextField");
         topPanel.add(textField);
-
+        //и кнопкой
         JButton button = new JButton("Get text!");
         button.setName("RunButton");
         topPanel.add(button);
         workFlow.add(topPanel,BorderLayout.NORTH);
-
+        //В центр добавляем центральную панель и делим ее border layout
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BorderLayout());
         workFlow.add(centerPanel,BorderLayout.CENTER);
-
+        //В центральную панель  сверху добавляем панель заголовков
         JPanel titlePanel = new JPanel();
         titlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         centerPanel.add(titlePanel,BorderLayout.NORTH);
-
+        //В панель заголовком добавляем метки
         JLabel header = new JLabel("Title: ");
         titlePanel.add(header);
-
-        title = new JLabel("");
-        title.setName("TitleLabel");
-        titlePanel.add(title);
+        titleLabel = new JLabel("");
+        titleLabel.setName("TitleLabel");
+        titlePanel.add(titleLabel);
+        //В центральную панель в центр добавляем тваблицу в скрол панеле
         JTable table = createTable();
         table.setBackground(Color.WHITE);
         centerPanel.add(new JScrollPane(table),BorderLayout.CENTER);
+        //В центральную панель снизу добавляем панель экспорта
+        JPanel exportPanel = new JPanel();
+        exportPanel.setLayout(new BorderLayout());
+        JTextField exportField = new JTextField("",20);
+        exportField.setName("ExportUrlTextField");
+        JButton exportButton = new JButton("Save");
+        exportButton.setName("ExportButton");
+        exportPanel.add(new JLabel("Export:"),BorderLayout.WEST);
+        exportPanel.add(exportField,BorderLayout.CENTER);
+        exportPanel.add(exportButton,BorderLayout.EAST);
+
+        centerPanel.add(exportPanel,BorderLayout.SOUTH);
         add(workFlow);
         button.addActionListener( event->{
             this.url=textField.getText();
             this.text=loadUrl(this.url);
-            this.textTitle=getTitle(text);
-            title.setText(textTitle);
-            updateTableData(table,getLinks(text,url,textTitle));
+            this.title =getTitle(text);
+            titleLabel.setText(title);
+            updateTableData(table,getLinks(text,url, title));
         });
-
+        exportButton.addActionListener(actionEvent -> {
+            try {
+                saveTable(exportField.getText(),getDataFromTable(table));
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        });
         setVisible(true);
     }
 
@@ -102,11 +106,10 @@ public class WebCrawler extends JFrame {
         if (url==null || url.isEmpty()) return null;
         try {
             URLConnection connection = new URL(url).openConnection();
-            if (!Objects.equals(connection.getContentType(),"text/html")) {
-                return null;
-            }
             try (InputStream input = connection.getInputStream()) {
                 return new String(input.readAllBytes(), StandardCharsets.UTF_8);
+            }catch (Exception exception){
+                return null;
             }
         }
         catch (IOException exception){
@@ -164,5 +167,18 @@ public class WebCrawler extends JFrame {
             url=root.substring(0,root.indexOf("://")+3)+url;
         }
         return url;
+    }
+    private static void saveTable(String filename,String[] data) throws IOException {
+        Files.writeString(Paths.get(filename),String.join("\n",data),StandardCharsets.UTF_8);
+    }
+    private static String[] getDataFromTable(JTable table){
+        List<String> result = new ArrayList<>();
+        DefaultTableModel model =(DefaultTableModel)table.getModel();
+        for(int row = 0; row<model.getRowCount();row++){
+            for (int col =0;col<model.getColumnCount();col++){
+                result.add((String) model.getValueAt(row,col));
+            }
+        }
+        return result.toArray(String[]::new);
     }
 }
