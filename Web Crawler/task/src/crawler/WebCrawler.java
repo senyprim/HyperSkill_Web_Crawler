51235a913f249955a35156b12f6ea6c4e24ea97f
senyprim <senyprim105@gmail.com>
2020-07-27 17:40:48 +1000

    Stage 4

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,5 +1,6 @@
 package crawler;
 
+import kotlin.text.Regex;
 import org.intellij.lang.annotations.JdkConstants;
 
 import javax.swing.*;
@@ -14,15 +15,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class WebCrawler extends JFrame {
+
+
+    JLabel title;
     private String text;
+    private String url;
+    private String textTitle;
+    private String[][] data;
+
+
     public WebCrawler() {
         super();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,61 +71,98 @@ public class WebCrawler extends JFrame {
         JLabel header = new JLabel("Title: ");
         titlePanel.add(header);
 
-        JLabel title = new JLabel("");
+        title = new JLabel("");
         title.setName("TitleLabel");
         titlePanel.add(title);
-
         JTable table = createTable();
         table.setBackground(Color.WHITE);
         centerPanel.add(new JScrollPane(table),BorderLayout.CENTER);
-
         add(workFlow);
-
         button.addActionListener( event->{
-                try {
-                    text=(loadUrl(textField.getText()));
-                    title.setText(getTitle(text));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            this.url=textField.getText();
+            this.text=loadUrl(this.url);
+            this.textTitle=getTitle(text);
+            title.setText(textTitle);
+            updateTableData(table,getLinks(text,url,textTitle));
         });
 
         setVisible(true);
     }
 
     private static String getTitle(String text) {
+        if (text==null || text.isEmpty()) return null;
         Matcher matcher = Pattern.compile("<title>([^<]*)"
                 , Pattern.CASE_INSENSITIVE|Pattern.MULTILINE)
                 .matcher(text);
-        matcher.find();
-        return matcher.group(1);
+        if (matcher.find()) return matcher.group(1);
+        return null;
     }
 
-    private static String loadUrl(String url) throws IOException {
-        try(InputStream input = new BufferedInputStream(new URL(url).openStream())){
-            return new String(input.readAllBytes(), StandardCharsets.UTF_8);
+    private static String loadUrl(String url) {
+        if (url==null || url.isEmpty()) return null;
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            if (!Objects.equals(connection.getContentType(),"text/html")) {
+                return null;
+            }
+            try (InputStream input = connection.getInputStream()) {
+                return new String(input.readAllBytes(), StandardCharsets.UTF_8);
+            }
+        }
+        catch (IOException exception){
+            return null;
         }
     }
+
     private JTable createTable(){
         String[] title = {"URL","Title"};
-        JTable jTable = new JTable();
         DefaultTableModel tableModel = new DefaultTableModel();
         tableModel.setColumnIdentifiers(title);
-        tableModel.addRow(new String[]{"",""});
-        return new JTable(tableModel);
+        JTable jTable = new JTable(tableModel);
+        jTable.setName("TitlesTable");
+        jTable.setEnabled(false);
+        return jTable;
     }
-    private Stream<String[]> getLinks(String text){
-        Matcher matcher = Pattern.compile("<\\s*a[^>]*href\\s*=\\s*['\"]+([^\\'\" >]+)[\\'\"]>(.*)\\<\\s*\\/a\\s*>"
-                ,Pattern.CASE_INSENSITIVE|Pattern.MULTILINE).matcher(text);
-        List<String[]> result = new ArrayList();
-        while (matcher.find()){
-            result.add(new String[]{matcher.group(1),matcher.group(2)});
+    private  void updateTableData(JTable table,String[][] data){
+        DefaultTableModel tableModel = (DefaultTableModel)table.getModel();
+        tableModel.setRowCount(0);
+
+        for (String[] line :data){
+            tableModel.addRow(line);
         }
-        return result.stream();
+        tableModel.fireTableDataChanged();
     }
-    
 
+    private static String[][] getLinks(String text,String url,String title) {
+        if (text==null || text.isEmpty()) return new String[0][0];
+        Matcher matcher = Pattern.compile("<\\s*a[^>]*href\\s*=\\s*['\"]+([^'\" >]+)['\"][^>]*>(.*)<\\s*/a\\s*>"
+                ,Pattern.CASE_INSENSITIVE|Pattern.MULTILINE).matcher(text);
+        List<String[]> result = new ArrayList<>();
+        result.add(new String[]{url,title});
+        while (matcher.find()){
+            String absoluteLink = getAbsoluteUrl(matcher.group(1),url);
+            title = getTitle(loadUrl(absoluteLink));
+            if (title==null || title.isEmpty()) continue;
+            result.add(new String[]{absoluteLink,title});
+        }
+        return result.toArray(String[][]::new);
+    }
 
+    private static String getAbsoluteUrl(String url,String root)
+    {
+        if (url==null || url.isEmpty() || root==null || root.isEmpty()) return null;
+        boolean isAbsoluteUrl = url.contains("/");
+        //Убираем ведущие и конечные слеши
+        url=(url.replace("/"," ")).trim().replace(" ","/");
+        boolean protocolExist = Pattern.matches("http[s]?://.*",url);
 
-
+        if (!isAbsoluteUrl){
+            String[] paths=root.split("/");
+            paths[paths.length-1]="";
+            url=String.join("/",paths)+url;
+        } else if (!protocolExist){
+            url=root.substring(0,root.indexOf("://")+3)+url;
+        }
+        return url;
+    }
 }
